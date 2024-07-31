4663efeaf5a4985da2f375d2db8be3c98449a9b6
aakashstha1 <aakash.cerestha90@gmail.com>
2024-07-31 22:07:31 +0545

    D

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import LoadingButton from "./LoadingButton";
 import axios from "axios";
 import { toast } from "react-toastify";
